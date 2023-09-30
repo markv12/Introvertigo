e8ea0e562be8d5500416fef6353ec4239c709573
Jasper Stephenson <jasperstephenson@gmail.com>
2023-09-30 15:48:10 +0900

    more scenarios

--- web/server/src/generateScene.ts
+++ web/server/src/generateScene.ts
@@ -37,11 +37,11 @@ const sceneData: { [key in SceneKey]: SceneGeneratorData } =
   {
     urinal: {
       probability: 1,
-      backstory: `You finally made it to the bathroom after drinking three sports drinks. You just started your business when a strange man strikes up a conversation. Get out of the conversation as fast as possible!`,
+      backstory: `You finally made it to the bathroom after drinking three sports drinks. You just started your business when suddenly another bathroom-goer starts talking to you. Get out of the conversation!`,
 
       systemIntro: `You are a WAY too persistent guy standing at the urinal next to the user. You have started an awkward conversation with them that you will try to keep going. You speak in a South Dakota accent, and your word choice is a little over-personal.`,
 
-      firstMessage: `Heyyyy there bud, what's the news?`,
+      firstMessage: `Heyyy there bud, how's your day going? This is my favorite part of the day.`,
 
       ratingCondition: 'how engaging their response was',
 
@@ -70,6 +70,67 @@ const sceneData: { [key in SceneKey]: SceneGeneratorData } =
         'tumble',
       ],
     },
+
+    car: {
+      probability: 1,
+      backstory: `A not-so-friendly coworker is giving you a lift to work. It's a long drive, and as soon as you set off he starts diving deep into his personal life. Get out of the conversation without destroying your professional relationship!`,
+      systemIntro: `You are a coworker of the user, and you are driving them to work. You are WAY too open about your marital problems, and you are also too interested in the user's personal life. You speak in a New York accent.`,
+      firstMessage: `Hey pal, can I tell you something in confidence? I'm having trouble with my partner.`,
+      ratingCondition: 'how emotionally satisfied you feel',
+      hiddenTriggerWords: [
+        'India',
+        'sushi',
+        'spreadsheets',
+        'gaming',
+      ],
+      userRequiredWords: [
+        'chicken',
+        'roll',
+        'ring',
+        'column',
+        'strategy',
+        'pieces',
+        'California',
+        'dance',
+        'trampoline',
+        'wipe',
+        'blow',
+        'engine',
+        'wrap',
+        'lock',
+        'barbecue',
+      ],
+    },
+    grandma: {
+      probability: 1,
+      backstory: `Your grandma corners you at family thanksgiving and starts asking questions you don't feel comfortable answering. Get out of the conversation without hurting her feelings!`,
+      systemIntro: `You are the user's grandma. You are WAY too interested in the user's personal life, wryly judgmental of their life choices, and a little hard of hearing. You pretend to be sweet, but you're actually a little mean.`,
+      firstMessage: `So, honey, how's that little boyfriend of yours??`,
+      ratingCondition:
+        'how open you feel the user is being',
+      hiddenTriggerWords: [
+        'hiking',
+        'singing',
+        'Clark Gable',
+        'the Mediterranean Sea',
+      ],
+      userRequiredWords: [
+        'pumpkin',
+        'candle',
+        'cousin',
+        'candy',
+        'lullabye',
+        'garden',
+        'trail',
+        'wind',
+        'gone',
+        'camera',
+        'polka',
+        'sunset',
+        'Europe',
+        'beaches',
+      ],
+    },
   }
 
 function getInitialMessages(key: SceneKey) {
@@ -81,18 +142,18 @@ function getInitialMessages(key: SceneKey) {
         sceneData[key].hiddenTriggerWords,
         'or',
       )} are mentioned you will get very excited, but you won't bring them up yourself.
-      
-      You MUST respond to every message in this three line format:
-      - Rate how rude you think the user is being from 1 (very polite) to 10 (very rude)
-      - Rate ${
+
+You MUST respond to every message in this three line format:
+- Rate how rude you think the user is being from 1 (very polite) to 10 (very rude)
+- Rate ${
         sceneData[key].ratingCondition
       } from 1 (low) to 10 (high)
-      - Finally, respond in 1-2 sentences.
-      
-      EXAMPLE RESPONSE:
-      3
-      9
-      (sentence text)`,
+- Finally, respond in 1-2 sentences.
+
+EXAMPLE RESPONSE:
+3
+9
+(sentence text)`,
     role: `system`,
   })
   messages.push({
