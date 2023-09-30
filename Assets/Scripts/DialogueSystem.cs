76e7aaf860d7c0c11f5d9b49e7c7776654402887
Scott Steffes <swsteffes@gmail.com>
2023-09-30 04:10:46 -0500

    add first message

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -9,25 +9,32 @@ public class DialogueSystem : MonoBehaviour {
     public TMP_Text outputDialogue;
     public TMP_Text whatYouSaidText;
     public Button enterButton;
+    public GameObject backstoryBG;
+    public TMP_Text backstoryText;
+    public Button backstoryBeginButton;
     private DialogueVertexAnimator dvaOutput;
     private DialogueVertexAnimator dvaWhatYouSaid;
 
+    private string firstMessage;
     private void Awake() {
         dvaOutput = new DialogueVertexAnimator(outputDialogue, null, PlayTalkSound);
         dvaWhatYouSaid = new DialogueVertexAnimator(whatYouSaidText, null, PlayTalkSound);
         enterButton.onClick.AddListener(Enter);
+        backstoryBeginButton.onClick.AddListener(() => { backstoryBG.SetActive(false); EnemyTalk(firstMessage); });
         GameRequestManager.Instance.GetGameScenario((GameScenario gameScenario) => {
             if(gameScenario != null) {
-                outputDialogue.text = gameScenario.backstory;
+                backstoryText.text = gameScenario.backstory;
+                backstoryBeginButton.gameObject.SetActive(true);
+                if(gameScenario.messages.Length > 0) {
+                    firstMessage = gameScenario.messages[gameScenario.messages.Length - 1].content;
+                }
             }
         });
     }
 
-    private Coroutine typeRoutine;
     private void Enter() {
         bool dialogueFinished = false;
         float stopTime = 0;
-        this.EnsureCoroutineStopped(ref typeRoutine);
 
         string inputText = mainInputField.text.Trim();
         if(!string.IsNullOrWhiteSpace(inputText) && inputText.Length <= 100) {
@@ -48,11 +55,15 @@ public class DialogueSystem : MonoBehaviour {
             while (!dialogueFinished || Time.time < stopTime) {
                 yield return null;
             }
-            dialogueFinished = false;
-            List<DialogueCommand> commands = DialogueUtility.ProcessInputString(rr.reply, out string processedMessage);
-            IEnumerator subRoutine = dvaOutput.AnimateTextIn(commands, processedMessage, 1, () => { dialogueFinished = true; stopTime = Time.time + 1f; });
-            while (subRoutine.MoveNext() && (!dialogueFinished || Time.time < stopTime)) yield return subRoutine.Current;
+            EnemyTalk(rr.reply);
         }
+    }
+
+    private Coroutine enemyTalkRoutine;
+    private void EnemyTalk(string message) {
+        this.EnsureCoroutineStopped(ref enemyTalkRoutine);
+        List<DialogueCommand> commands = DialogueUtility.ProcessInputString(message, out string processedMessage);
+        enemyTalkRoutine = StartCoroutine(dvaOutput.AnimateTextIn(commands, processedMessage, 1, null));
     }
 
     private void PlayTalkSound(float pitchCenter) {
