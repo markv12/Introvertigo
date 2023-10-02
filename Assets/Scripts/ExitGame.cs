d3a88196bd4718745868389f59017eb4816f2717
Scott Steffes <swsteffes@gmail.com>
2023-10-02 02:17:07 -0500

    add exit buttons

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitGame : MonoBehaviour
-{
-  public void Exit()
-  {
+public class ExitGame : MonoBehaviour {
+#if UNITY_WEBGL
+    private void Awake() {
+        Destroy(gameObject);
+    }
+#endif
+    public void Exit() {
 #if UNITY_EDITOR
         // Application.Quit() does not work in the editor so
         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
@@ -13,5 +16,5 @@ public class ExitGame : MonoBehaviour
 #else
     Application.Quit();
 #endif
-  }
+    }
 }
