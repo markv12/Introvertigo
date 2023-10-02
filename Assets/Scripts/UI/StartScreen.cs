29c176114fe53b5cfd235a542bd4ddc01adfc15a
Scott Steffes <swsteffes@gmail.com>
2023-10-02 07:29:36 -0500

    Car Stuff

--- Assets/Scripts/UI/StartScreen.cs
+++ Assets/Scripts/UI/StartScreen.cs
@@ -1,38 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StartScreen : MonoBehaviour
-{
+public class StartScreen : MonoBehaviour {
     public Button startButton;
     public Button closeButton;
 
     public GameObject howToPlayElement;
 
-    private void Awake()
-    {
+    private void Awake() {
         startButton.onClick.AddListener(StartGame);
     }
 
     private bool started = false;
-    private void StartGame()
-    {
-        if (!started)
-        {
+    private void StartGame() {
+        if (!started) {
             started = true;
             GameFlowManager.StartGame();
         }
     }
 
-    public void ShowHowToPlay()
-    {
+    public void ShowHowToPlay() {
         howToPlayElement.gameObject.SetActive(true);
-        closeButton.gameObject.SetActive(false);
+        if (closeButton != null) {
+            closeButton.gameObject.SetActive(false);
+        }
     }
 
-    public void HideHowToPlay()
-    {
+    public void HideHowToPlay() {
         howToPlayElement.gameObject.SetActive(false);
-        closeButton.gameObject.SetActive(true);
+        if (closeButton != null) {
+            closeButton.gameObject.SetActive(true);
+        }
     }
-
 }
